6ea86e764df078f26eb722a09d4217d2013a6eca
Aditya Kumar <58542359+addiraws@users.noreply.github.com>
2021-02-24 21:22:07 +0530

    Update equation of line.dart

--- equation of line.dart
+++ equation of line.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 void main() {
   var x1 = 0, x2 = 0, y1 = 0, y2 = 0; //intiial values
-
+  stdout.write('Enter the first co-ordinate.');
+  print('(X,Y)');
   x1 = int.parse(stdin.readLineSync());
   y1 = int.parse(stdin.readLineSync());
+  print('Enter the second co-ordinate.');
   x2 = int.parse(stdin.readLineSync());
   y2 = int.parse(stdin.readLineSync());
 
@@ -12,9 +14,8 @@ void main() {
   line.printLine();
 }
 
-
 class Point {
-  int x = 0, y = 0;
+  int x, y;
   Point({this.x, this.y});
 }
 
@@ -22,15 +23,29 @@ class Line {
   var point = Point(x: 0, y: 0);
   var secondPoint = Point(x: 0, y: 0);
   var slope;
+  var flag;
 
   Line({this.point, this.secondPoint}) {
-    slope = (point.y - secondPoint.y) / (point.x - secondPoint.y)!=0?(point.x - secondPoint.y):1;
-  
-  }5
+    slope = (point.y - secondPoint.y) / (point.x - secondPoint.x);
+    if (slope > 0 || slope < 0) //for checking error of 0/0 error
+      flag = 1;
+    else
+      flag = -1;
+  }
   void printLine() {
-    slope == 0
-        ? print('y=${secondPoint.y}')
-        : print(
-            'y=${slope * secondPoint.x}x+${slope * secondPoint.x + secondPoint.y}');
+    if (flag == 1 && secondPoint.x != point.x) {
+      stdout.write('y=');
+      slope == 0
+          ? stdout.write('${secondPoint.y}')
+          : (-slope * secondPoint.x + secondPoint.y) >= 0
+              ? stdout
+                  .write('${slope}x+${-slope * secondPoint.x + secondPoint.y}')
+              : stdout
+                  .write('${slope}x${-slope * secondPoint.x + secondPoint.y}');
+    } else if (secondPoint.x == point.x) {
+      print('x=${secondPoint.x}');
+    } else
+      print(
+          'Line cannot be printed. It\'s a point co-odinate that you entered.');
   }
 }
